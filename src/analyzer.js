e8010f7db8eb4b63fe5aff76ba5f3eccdca623c9
sandymmora <sandy.mmora@gmail.com>
2024-03-04 12:49:59 -0600

    Correcciones en funciones recuento y suma de numeros

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,4 +1,3 @@
-
 const analyzer = {  
 
   getWordCount: (text) => { 
@@ -22,8 +21,8 @@ const analyzer = {
   getCharacterCountExcludingSpaces: (text) => {
     let characterCountExcludingSpaces = 0;
     for (let i = 0; i < text.length ; i++) {
-      if(((text.charCodeAt(i) >= 65 && text.charCodeAt(i) <= 90) | (text.charCodeAt(i) >= 97 && text.charCodeAt(i) <= 122))) {
-        characterCountExcludingSpaces ++;
+      if(((text.charCodeAt(i) >= 65 && text.charCodeAt(i) <= 90) | (text.charCodeAt(i) >= 97 && text.charCodeAt(i) <= 122))| (text.charCodeAt(i) >= 48 && text.charCodeAt(i) <= 57)){
+        characterCountExcludingSpaces++;
       }
     }
     return characterCountExcludingSpaces;
@@ -39,40 +38,48 @@ const analyzer = {
         wordCount++;   
         wordsLength+=words[i].length;
       }
-     
     }
     let averageWordLength = wordsLength / wordCount;
     averageWordLength = parseFloat(averageWordLength.toFixed(2));
-
     return averageWordLength;
     //console.log(suma);
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string` 
   },
 
   getNumberCount: (text) => {
+    const  words = text.split(" ");
     let numberCount = 0;
-    const words = text.split(" ");
-    //console.log(words, "palabras");
-    console.log(words[0].charAt(2));
-    for(let i=0; i<text.length; i++) {
-      if(text.charCodeAt(i) >= 48 && text.charCodeAt(i) <= 57) {  
-        numberCount++;
+    for(let i = 0; i<words.length; i++) {
+      let word = words[i];
+      if(word.length !== 0){
+        if(word[word.length-1]==='.'){
+          word = word.slice(0, -1);
+        }
+        if(!isNaN(word)){
+          numberCount++
+        }
       }
-    }
+    } 
     return numberCount;
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
   },
 
   getNumberSum: (text) => {
-    let sum = 0;
-    for(let i=0; i<text.length; i++) {
-      if(text.charCodeAt(i) >= 48 && text.charCodeAt(i) <= 57) {
-        const numberSum = parseInt(text[i]); 
-        sum = sum + numberSum;
+    const  words = text.split(" ");
+    let numberSum = 0;
+    for(let i = 0; i<words.length; i++) {
+      if(words[i].length !== 0) {
+        let word = words[i]
+        if(word[word.length-1]==='.'){
+          word = word.slice(0, -1);
+        }
+        if(!isNaN(word)){
+          const number = Number(word);
+          numberSum += number;
+        }
       }
-    }
-    return sum;
-    //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    } 
+    return numberSum;
   },
 };
 
--- src/index.js
+++ src/index.js
@@ -1,32 +1,45 @@
 import analyzer from './analyzer.js';
 
+const primerMetrica = document.querySelector("li:nth-child(1)");
+const segundaMetrica = document.querySelector("li:nth-child(2)");
+const tercerMetrica = document.querySelector("li:nth-child(3)");
+const cuartaMetrica = document.querySelector("li:nth-child(4)");
+const quintaMetrica = document.querySelector("li:nth-child(5)");
+const sextaMetrica = document.querySelector("li:nth-child(6)");
+
+function limpiarMetricas(){
+  primerMetrica.innerText= "Palabras: " +  0;
+  segundaMetrica.innerText= "Caracteres: " + 0;
+  tercerMetrica.innerText  = "Caracteres sin espacios: " + 0;
+  cuartaMetrica.innerText  = "Números: " + 0;
+  quintaMetrica.innerText  = "Suma Números: " + 0;
+  sextaMetrica.innerText  = "Longitud media: " + 0;
+  inputText.value = "";
+}
+
 function analizarTexto() {
   const text = inputText.value; 
   const words = analyzer.getWordCount(text);
-  const primerMetrica = document.querySelector("li:nth-child(1)");
-  primerMetrica.innerText= "Palabras: " +  words;
+  primerMetrica.textContent= "Palabras: " +  words;
   
   const caracteres = analyzer.getCharacterCount(text);
-  const segundaMetrica = document.querySelector("li:nth-child(2)");
-  segundaMetrica.innerText= "Caracteres: " + caracteres;
+  segundaMetrica.textContent = "Caracteres: " + caracteres;
+
   const caracteresSinEspacios = analyzer.getCharacterCountExcludingSpaces(text);
-  const tercerMetrica = document.querySelector("li:nth-child(3)");
-  tercerMetrica.innerText  = "Caracteres sin espacios: " + caracteresSinEspacios;
+  tercerMetrica.textContent  = "Caracteres sin espacios: " + caracteresSinEspacios;
   
   const numeros = analyzer.getNumberCount(text);
-  const cuartaMetrica = document.querySelector("li:nth-child(4)");
-  cuartaMetrica.innerText  = "Números: " + numeros;
+  cuartaMetrica.textContent  = "Números: " + numeros;
   
   const sumaNumeros = analyzer.getNumberSum(text);
-  const quintaMetrica = document.querySelector("li:nth-child(5)");
-  quintaMetrica.innerText  = "Suma Números: " + sumaNumeros;
-  
+  quintaMetrica.textContent  = "Suma Números: " + sumaNumeros;
+
   const promedioPalabras = analyzer.getAverageWordLength(text);
-  const sextaMetrica = document.querySelector("li:nth-child(6)");
-  sextaMetrica.innerText  = "Longitud media: " + promedioPalabras;
+  sextaMetrica.textContent  = "Longitud media: " + promedioPalabras;
 }
 
 const inputText = document.querySelector("textarea");
 inputText.addEventListener("input", analizarTexto) 
 
-
+const boton = document.getElementById("reset-button");
+boton.addEventListener("click", limpiarMetricas);
